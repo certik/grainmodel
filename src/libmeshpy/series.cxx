b2c66a43e187f55f2853d535e8dd2890814c4ed0
ondrej.certik <ondrej.certik@1a83ecf4-dd27-0410-977f-e125965af2d1>
2007-02-01 17:09:04 +0000

    python bindings to libmesh

---
+++ src/libmeshpy/series.cxx
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include "series.h"
+
+double doubleSum(double* series, int size) {
+  double result = 0.0;
+  for (int i=0; i<size; ++i) result += series[i];
+  return result;
+}
+
+void doubleOnes(double* array, int size) {
+  for (int i=0; i<size; ++i) array[i] = 1.0;
+}
+
+//#include "libmesh.h"
+//#include "mesh.h"
+
+void mesh()
+{
+    std::cout << "hello" << std::endl;
+
+/*    libMesh::init (0, NULL);
+    {    
+        const unsigned int dim = 3;
+        Mesh mesh(dim);
+        //mesh.read (argv[3]);
+        mesh.print_info();
+        //mesh.write (argv[4]);
+    }
+    return libMesh::close();*/
+}
+
+
---
+++ src/libmeshpy/series.h
@@ -0,0 +1,9 @@
+#ifndef SERIES_H
+#define SERIES_H
+
+double doubleSum( double* series, int size);
+void doubleOnes(  double* array, int size);
+
+void mesh();
+
+#endif

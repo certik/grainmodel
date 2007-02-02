ff042a5826ff1656470c52ac42118f3d20ac94c8
ondrej.certik <ondrej.certik@1a83ecf4-dd27-0410-977f-e125965af2d1>
2007-02-02 13:40:00 +0000

    libmesh links ok, but there are still some symbols missing

--- src/libmeshpy/series.cxx
+++ src/libmeshpy/series.cxx
@@ -13,14 +13,16 @@ void doubleOnes(double* array, int size) {
   for (int i=0; i<size; ++i) array[i] = 1.0;
 }
 
-//#include "libmesh.h"
-//#include "mesh.h"
+#include "libmesh.h"
+#include "mesh.h"
 
 void mesh()
 {
     std::cout << "hello" << std::endl;
 
-/*    libMesh::init (0, NULL);
+    int argc=0;
+    char **argv=NULL;
+    libMesh::init (argc, argv);
     {    
         const unsigned int dim = 3;
         Mesh mesh(dim);
@@ -28,7 +30,7 @@ void mesh()
         mesh.print_info();
         //mesh.write (argv[4]);
     }
-    return libMesh::close();*/
+    libMesh::close();
 }
 
 

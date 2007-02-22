fcc909e1e292607da625c58a1cb5e015ce90f61c
ondrej.certik <ondrej.certik@1a83ecf4-dd27-0410-977f-e125965af2d1>
2007-02-22 17:15:36 +0000

    working python wrappers

--- trunk/src/libmeshpy/testlibmesh.py
+++ trunk/src/libmeshpy/testlibmesh.py
@@ -16,6 +16,35 @@ myArray = N.zeros(5,'d')
 libmeshpy.doubleOnes(myArray)
 print myArray, N.array([1.,1.,1.,1.,1.])
 
-libmeshpy.mesh("../../tmp/in.xda")
+print "starting"
+#libmeshpy.mesh("../../tmp/in.xda")
+
+import numpy
+class load:
+    def __init__(self,fname):
+        self.m=libmeshpy.loadmatrices(fname)
+    def loadmatrix(self):
+        x=numpy.zeros((4,4))
+        for j in range(4):
+            for i in range(4):
+                x[i,j]=self.m.readfloat()
+        return x
+    def loadindices(self):
+        x=numpy.zeros((4),dtype=int)
+        for i in range(4):
+            x[i]=self.m.readint()
+        return x
+    def loadvector(self):
+        x=numpy.zeros((4))
+        for i in range(4):
+            x[i]=self.m.readfloat()
+        return x
+
+l=load("../../tmp/matrices")
+for i in range(10):
+    print l.loadmatrix()
+    print l.loadindices()
+    print l.loadvector()
+    print l.loadindices()
 
 print "ok, we are done."

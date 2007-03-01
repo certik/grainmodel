e28b18328d0c6b0b88567b9f527d3a56ce66873f
ondrej.certik <ondrej.certik@1a83ecf4-dd27-0410-977f-e125965af2d1>
2007-03-01 19:44:00 +0000

    correct handling of gradients

--- trunk/femlib/src/libmeshpy.h
+++ trunk/femlib/src/libmeshpy.h
@@ -13,10 +13,16 @@ void mesh(const std::string& fmesh, const std::string& fmatrices,
     const std::string& fboundaries, Updater *up);
 
 void grad(const std::string& meshfile, double* x, int xsize, 
-        double* g, int gsize, Updater *up);
+        double* gx, int gxsize,
+        double* gy, int gysize,
+        double* gz, int gzsize,
+        Updater *up);
 
 double integ(const std::string& meshfile, const std::string& fboundaries, 
-        double* x, int xsize, int b, Updater *up);
+        double* x, int xsize,
+        double* y, int ysize,
+        double* z, int zsize,
+        int b, Updater *up);
 
 class loadmatrices
 //currently Ax=F

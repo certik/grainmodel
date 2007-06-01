0fcec79d97eaa01b27fdce0df2b44f8805cf231f
ondrej.certik <ondrej.certik@1a83ecf4-dd27-0410-977f-e125965af2d1>
2007-06-01 23:52:32 +0000

    polishing setup.py

--- trunk/femlib/src/setup.py
+++ trunk/femlib/src/setup.py
@@ -1,29 +1,15 @@
 #! /usr/bin/env python
 
-# System imports
 from distutils.core import *
-from distutils      import sysconfig
-import glob
-import os
-
-# Third-party modules - we depend on numpy for everything
 import numpy
 
-# _Series extension module
-lib="/home/ondra/libmeshpetscpackage/libs/libmesh2"
-libmesh=os.path.dirname(glob.glob(lib+"/lib/*/libmesh.so")[0])
-libmesh_contrib=os.path.dirname(glob.glob(lib+
-    "/contrib/lib/*/liblaspack.so")[0])
-libmesh_inc=glob.glob(lib+"/include/*")
-libpaths=[libmesh,libmesh_contrib]
 _libmeshpy = Extension("_libmeshpy",
-                    ["libmeshpy_wrap.cxx",
-                     "libmeshpy.cxx"],
-                    include_dirs = [numpy.get_include()]+libmesh_inc,
-                    library_dirs=libpaths,
-                    runtime_library_dirs=libpaths,
+                    ["libmeshpy_wrap.cxx", "libmeshpy.cxx"],
+                    include_dirs = [numpy.get_include(),"/usr/include/libmesh",
+                        "/usr/include/mpi","/usr/include/petsc"],
                     extra_compile_args=["-O2"],
-                    libraries = ["mesh","laspack"]
+                    libraries = ["mesh","petsc","petscdm","petscksp","petscmat",
+                        "petscsnes","petscts","petscvec"]
                     )
 
 setup(name        = "libmeshpy",

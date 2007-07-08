b140b585fb4fc263ed0b9c801d07c41ca35a8593
ondrej.certik <ondrej.certik@1a83ecf4-dd27-0410-977f-e125965af2d1>
2007-07-08 00:18:14 +0000

    Cleaning works

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-from distutils.core import setup, Extension
+from distutils.core import setup, Extension, Command
 import numpy
 from sys import argv
 
@@ -8,6 +8,31 @@ if argv[1] == "build":
     argv[1] = "build_ext"
 if argv[1] == "build_ext":
     argv.insert(2, "--swig-opts=-c++")
+
+class clean(Command):
+    """Cleans *.pyc and debian trashs, so you should get the same copy as 
+    is in the svn.
+    """
+    
+    description = "Clean everything"
+    user_options = [("all","a","the same")]  
+
+    def initialize_options(self):  
+        self.all = None
+    
+    def finalize_options(self):   
+        pass
+
+    def run(self):
+        import os
+        os.system("py.cleanup")
+        os.system("rm -f python-build-stamp-2.4")
+        os.system("rm -f MANIFEST")
+        os.system("rm -rf build")
+        os.system("rm -rf dist")
+        os.system("rm -f femlib/libmeshpy/libmeshpy.py")
+        os.system("rm -f femlib/libmeshpy/libmeshpy_wrap.cpp")
+        os.system("rm -f femlib/libmeshpy/libmeshpy_wrap.h")
 
 extdir="femlib/libmeshpy"
 _libmeshpy = Extension("femlib.libmeshpy._libmeshpy",
@@ -25,4 +50,7 @@ setup(name        = "libmeshpy",
       packages = ["femlib", "geom", "femlib.libmeshpy"],
       #py_modules  = ["femlib.libmeshpy"],
       ext_modules = [_libmeshpy],
+      cmdclass    = {
+                     'clean' : clean, 
+                     },
       )
